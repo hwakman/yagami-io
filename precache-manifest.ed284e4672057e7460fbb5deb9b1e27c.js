76f0c5b7ac9e705fd780c97df7062ea783f982c0
TOR <chayoud.p@arms-thai.com>
2018-11-30 18:00:28 +0700

    Updates

---
+++ precache-manifest.ed284e4672057e7460fbb5deb9b1e27c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4df5a3deaf8102d01a7b",
+    "url": "/yagami-oi/static/css/main.30595e1d.chunk.css"
+  },
+  {
+    "revision": "4df5a3deaf8102d01a7b",
+    "url": "/yagami-oi/static/js/main.4df5a3de.chunk.js"
+  },
+  {
+    "revision": "874addbee8f83cfe797d",
+    "url": "/yagami-oi/static/css/1.275ace4f.chunk.css"
+  },
+  {
+    "revision": "874addbee8f83cfe797d",
+    "url": "/yagami-oi/static/js/1.874addbe.chunk.js"
+  },
+  {
+    "revision": "daab5e048248cd67c05f",
+    "url": "/yagami-oi/static/js/runtime~main.daab5e04.js"
+  },
+  {
+    "revision": "92abbc5437f2e5214d787029b68efaa5",
+    "url": "/yagami-oi/static/media/chara.92abbc54.jpg"
+  },
+  {
+    "revision": "bf8712cb1535b8b6da9ddfedfe8527e9",
+    "url": "/yagami-oi/static/media/baner.bf8712cb.jpg"
+  },
+  {
+    "revision": "d32ed0cd5f6369b34628acf4055e0ac3",
+    "url": "/yagami-oi/static/media/yagami.d32ed0cd.jpg"
+  },
+  {
+    "revision": "5946266b546b3984aa062f8acb9ef077",
+    "url": "/yagami-oi/index.html"
+  }
+];
